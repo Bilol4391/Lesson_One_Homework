4c10ec3a528ca958ba735ab76411a99eacde28ba
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-08 00:03:14 +0500

    Masala - 3

--- lib/main.dart
+++ lib/main.dart
@@ -175,7 +175,7 @@
 //   }
 // }
 
-/// MASALA - 2
+/// Homework - 2
 
 // import 'package:flutter/material.dart';
 //
@@ -307,6 +307,107 @@
 //             ],
 //           ),
 //         ),
+//       ),
+//     );
+//   }
+// }
+
+/// MASALA - 3
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.black12,
+//         child: Center(
+//             child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Container(
+//               width: 80,
+//               height: 350,
+//               decoration: const BoxDecoration(
+//                   color: Colors.deepOrangeAccent,
+//                   borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(16),
+//                       bottomLeft: Radius.circular(16))),
+//               child: Center(
+//                   child: Text("B",
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(
+//                         decoration: TextDecoration.none,
+//                         color: Colors.white,
+//                       ))),
+//             ),
+//             Container(
+//                 width: 80,
+//                 height: 350,
+//                 color: Colors.cyanAccent,
+//                 child: Center(
+//                   child: Text(
+//                     "I",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 )),
+//             Container(
+//               height: 350,
+//               width: 80,
+//               color: Colors.pink,
+//               child: Center(
+//                 child: Text(
+//                   "L",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 350,
+//               width: 80,
+//               color: Colors.orange,
+//               child: Center(
+//                 child: Text(
+//                   "O",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height: 350,
+//               width: 80,
+//               decoration: const BoxDecoration(
+//                 color: Colors.teal,
+//                   borderRadius: BorderRadius.only(
+//                 bottomRight: Radius.circular(16),
+//                 topRight: Radius.circular(16),
+//               )),
+//               child: Center(
+//                 child: Text(
+//                   "L",
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                       decoration: TextDecoration.none, color: Colors.white),
+//                 ),
+//               ),
+//             )
+//           ],
+//         )),
 //       ),
 //     );
 //   }
