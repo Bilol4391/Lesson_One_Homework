9d4efff12a40e5ee270b1bc273ec3118c2eccf2f
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-07 23:13:56 +0500

    Masala - 2

--- lib/main.dart
+++ lib/main.dart
@@ -50,127 +50,264 @@
 //     );
 //
 
-//! Homework - 1
+/// Homework - 1
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.black12,
+//         child: Center(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Container(
+//                   height: 300,
+//                   width: 70,
+//                   decoration: const BoxDecoration(
+//                       borderRadius: BorderRadius.only(
+//                           topLeft: Radius.circular(16),
+//                           bottomLeft: Radius.circular(16)),
+//                       color: Colors.blueAccent),
+//                   child: Center(
+//                       child: const Text(
+//                     "F",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//               Container(
+//                   height: 300,
+//                   width: 60,
+//                   decoration: const BoxDecoration(
+//                     color: Colors.blue,
+//                   ),
+//                   child: Center(
+//                       child: const Text(
+//                     "L",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//               Container(
+//                   height: 300,
+//                   width: 60,
+//                   decoration: const BoxDecoration(
+//                     color: Colors.lightBlue,
+//                   ),
+//                   child: Center(
+//                       child: const Text(
+//                     "U",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//               Container(
+//                   height: 300,
+//                   width: 60,
+//                   decoration: const BoxDecoration(
+//                     color: Colors.cyan,
+//                   ),
+//                   child: Center(
+//                       child: const Text(
+//                     "T",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//               Container(
+//                   height: 300,
+//                   width: 60,
+//                   decoration: const BoxDecoration(
+//                     color: Colors.white70,
+//                   ),
+//                   child: Center(
+//                       child: const Text(
+//                     "T",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//               Container(
+//                   height: 300,
+//                   width: 60,
+//                   decoration: const BoxDecoration(
+//                     color: Colors.cyanAccent,
+//                   ),
+//                   child: Center(
+//                       child: const Text(
+//                     "E",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//               Container(
+//                   height: 300,
+//                   width: 60,
+//                   decoration: const BoxDecoration(
+//                     borderRadius: BorderRadius.only(
+//                         topRight: Radius.circular(16),
+//                         bottomRight: Radius.circular(16)),
+//                     color: Colors.indigo,
+//                   ),
+//                   child: Center(
+//                       child: const Text(
+//                     "R",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ))),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-void main() {
-  runApp(const MyApp());
-}
+/// MASALA - 2
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.black12,
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  height: 300,
-                  width: 70,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(16),
-                          bottomLeft: Radius.circular(16)),
-                      color: Colors.blueAccent),
-                  child: Center(
-                      child: const Text(
-                    "F",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-              Container(
-                  height: 300,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                      child: const Text(
-                    "L",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-              Container(
-                  height: 300,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    color: Colors.lightBlue,
-                  ),
-                  child: Center(
-                      child: const Text(
-                    "U",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-              Container(
-                  height: 300,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    color: Colors.cyan,
-                  ),
-                  child: Center(
-                      child: const Text(
-                    "T",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-              Container(
-                  height: 300,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    color: Colors.white70,
-                  ),
-                  child: Center(
-                      child: const Text(
-                    "T",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-              Container(
-                  height: 300,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    color: Colors.cyanAccent,
-                  ),
-                  child: Center(
-                      child: const Text(
-                    "E",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-              Container(
-                  height: 300,
-                  width: 60,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(16),
-                        bottomRight: Radius.circular(16)),
-                    color: Colors.indigo,
-                  ),
-                  child: Center(
-                      child: const Text(
-                    "R",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        decoration: TextDecoration.none, color: Colors.white),
-                  ))),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Container(
+//         color: Colors.black12,
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 decoration: const BoxDecoration(
+//                     color: Colors.indigo,
+//                     borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(16),
+//                       topRight: Radius.circular(16),
+//                     )),
+//                 child: Center(
+//                   child: Text(
+//                     "F",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 color: Colors.blueAccent,
+//                 child: Center(
+//                   child: Text(
+//                     "L",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 color: Colors.cyan,
+//                 child: Center(
+//                   child: Text(
+//                     "U",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 color: Colors.cyanAccent,
+//                 child: Center(
+//                   child: Text(
+//                     "T",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 color: Colors.lightBlue,
+//                 child: Center(
+//                   child: Text(
+//                     "T",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 color: Colors.blueAccent,
+//                 child: Center(
+//                   child: Text(
+//                     "E",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//               Container(
+//                 width: 350,
+//                 height: 100,
+//                 decoration: const BoxDecoration(
+//                     color: Colors.indigo,
+//                     borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16),
+//                     bottomRight: Radius.circular(16)
+//                   )
+//                 ),
+//                 child: Center(
+//                   child: Text(
+//                     "R",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                         decoration: TextDecoration.none, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
