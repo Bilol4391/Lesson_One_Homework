f97bc45387e2fec3149fa6c70bdcf577d423efbc
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-08 23:05:26 +0500

    Masala - 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:myapp/Lesson_Two_Task_One.dart';
+import 'package:myapp/Lesson_Two_Task_Two.dart';
 
 void main() {
-  runApp(const TaskOne());
+  runApp(const TaskTwo());
 }
 
