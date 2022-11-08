6a8f93250d12533dd448d91a5b6ceb610b5dd196
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-09 01:25:20 +0500

    Uyga Vazifani 1,2,3,4,5 chi misollari

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/Lesson_Two_Task_Five.dart';
+import 'package:myapp/Lesson_Two_Task_Four.dart';
 import 'package:myapp/Lesson_Two_Task_One.dart';
+import 'package:myapp/Lesson_Two_Task_Six.dart';
+import 'package:myapp/Lesson_Two_Task_Three.dart';
+import 'package:myapp/Lesson_Two_Task_Two.dart';
 
 
 void main() {
-  runApp(const TaskOne());
+  runApp(const TaskSix());
 }
